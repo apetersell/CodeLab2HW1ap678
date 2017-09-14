50b0aecbb16d7701fbc8d94aacc6a317a12ca552
Andrew Petersell <andrewpetersell@gmail.com>
2017-09-13 21:37:44 -0400

    Scoring but actually though

---
+++ Week 1 Homework/Assets/Scripts/Scoring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour {
+
+	public int score;
+	public Text scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scoreDisplay.text = score.ToString ();
+		
+	}
+
+	public void increaseScore ()
+	{
+		score++;
+	}
+}
