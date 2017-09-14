28998930da66d89089abfe77834a1c0f62d8a7b9
Andrew Petersell <andrewpetersell@gmail.com>
2017-09-13 20:01:53 -0400

    BallBounce

---
+++ Week 1 Homework/Assets/Scripts/DangerBall.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerBall : MonoBehaviour {
+
+	public float xSpeed;
+	public float ySpeed;
+	public float maxSpeed; 
+	public float minSpeed;
+	public int colorNum;
+	public Sprite [] colors; 
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+
+	void Awake () 
+	{
+		findNewSpeed ();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		movement ();
+		
+	}
+
+	void findNewSpeed ()
+	{
+		xSpeed = Random.Range (minSpeed, maxSpeed); 
+		ySpeed = Random.Range (minSpeed, maxSpeed); 
+	}
+
+	void movement ()
+	{
+		sr.sprite = colors [colorNum];
+		rb.velocity = new Vector2 (xSpeed, ySpeed);
+
+
+	}
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.tag == "WallX")
+		{
+			xSpeed = xSpeed * -1;
+		}
+		if (coll.gameObject.tag == "WallY")
+		{
+			ySpeed = ySpeed * -1;
+		}
+		colorNum = Random.Range (0, 4);
+	}
+
+
+}
---
+++ Week 1 Homework/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Vector3 mousePos;
+
+	public bool red;
+	public bool yellow;
+	public bool green;
+	public bool blue; 
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		colorHanlde ();
+
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.position = mousePos;
+		sr = GetComponent<SpriteRenderer> ();
+		
+	}
+
+	void colorHanlde()
+	{
+//		if (red) {
+//			sr.color = Color.red;
+//		}
+//		if (yellow) {
+//			sr.color = Color.yellow;
+//		}
+//		if (green) {
+//			sr.color = Color.green;
+//		}
+//		if (blue) {
+//			sr.color = Color.blue;
+//		}
+//
+//		if (Input.GetKeyDown (KeyCode.A)) 
+//		{
+//			red = true;
+//			yellow = false;
+//			green = false;
+//			blue = false;
+//		}
+//		if (Input.GetKeyDown (KeyCode.S)) 
+//		{
+//			yellow = true;
+//			red = false;
+//			green = false;
+//			blue = false;
+//		}
+//		if (Input.GetKeyDown (KeyCode.D))
+//		{
+//			green = true;
+//			red = false;
+//			yellow = false;
+//			blue = false;
+//		}
+//		if (Input.GetKeyDown (KeyCode.F))
+//		{
+//			blue = true;
+//			red = false;
+//			yellow = false;
+//			green = false;
+//		}
+	}
+}
